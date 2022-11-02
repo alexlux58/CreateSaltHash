750917edab40576f7d7a5ffc4f09d7175c97d4df
alexlux58 <alexlux@live.com>
2022-11-02 03:33:05 -0700

    Completed salt and hash button functions

--- createSaltHash.js
+++ createSaltHash.js
@@ -1,5 +1,3 @@
-let passwordInputElement = document.getElementById("password");
-
 function genPassword() {
   let chars =
     "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -18,4 +16,27 @@ function copyPassword() {
   let copyText = document.getElementById("password");
   copyText.select();
   document.execCommand("copy");
+}
+
+function saltPassword() {
+  let saltyPassword = document.getElementById("SHpassword").value + "salt";
+  document.getElementById("SHpassword").value = saltyPassword;
+}
+
+// Convert to 32bit integer
+function hashPassword() {
+  string = document.getElementById("SHpassword").value;
+  let hash = 0;
+
+  if (string.length == 0) {
+    document.getElementById("SHpassword").value = hash;
+  }
+
+  for (i = 0; i < string.length; i++) {
+    char = string.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash = hash & hash;
+  }
+
+  document.getElementById("SHpassword").value = hash;
 }
